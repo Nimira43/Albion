7fc92aaeeb1c0e24ca4231376a826b3a199621db
Nimira43 <n.rabone72@aol.com>
2025-09-06 22:07:54 +0100

    Setting up grid responsiveness

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -11,7 +11,7 @@ const ProductList = ({
         {title}
       </h2>
       { data.length > 0 ? (
-        <div className='grid grid-cols-1'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
           Product Grid
         </div>
       ) : (
