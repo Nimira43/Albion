b7bd86685e39b17935043f6414ee169487610018
Nimira43 <n.rabone72@aol.com>
2025-09-07 00:13:01 +0100

    seed.ts - 2

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 import sampleData from './sample-data'
 
+async function main() {
+  const prisma = new PrismaClient()
+  await prisma.product.deleteMany() 
+}
 
+main()
