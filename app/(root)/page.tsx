360a0b5b93b1ba457208bd2f35eedcd791050c04
Nimira43 <n.rabone72@aol.com>
2025-09-06 20:50:45 +0100

    Testing loader - all good

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-const HomePage = () => {
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms)) 
+
+const HomePage = async () => {
+  await delay(2000)
+
   return ( 
    <div>Albion</div>
     
