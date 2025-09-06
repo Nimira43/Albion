0dc2db37a1f132cad20ece4db4c3f9415b552004
Nimira43 <n.rabone72@aol.com>
2025-09-06 20:47:43 +0100

    Loader image set up.

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,22 @@ import loader from '@/assets/loader.gif'
 
 const LoadingPage = () => {
   return ( 
-    <div>Loading</div>
+    <div 
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Image 
+        src={loader}
+        height={150}
+        width={150}
+        alt='Loading...'
+      />
+    </div>
    )
 }
  
