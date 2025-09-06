e552cbe5c49d6f45d3b0c3f4f90b6240e20fc2fc
Nimira43 <n.rabone72@aol.com>
2025-09-06 20:56:13 +0100

    Brought in imports for NotFoundPage

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { APP_NAME } from '@/lib/constants'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+const NotFoundPage = () => {
+  return ( 
+    <div>Not Found</div>
+   )
+}
+ 
+export default NotFoundPage
