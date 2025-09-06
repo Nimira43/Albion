6e09edff3d01512eb5906d521dd342403d3c7dff
Nimira43 <n.rabone72@aol.com>
2025-09-06 20:12:25 +0100

    useEffect with setMounted

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -9,8 +9,15 @@ import { useTheme } from 'next-themes'
 
 const ModeToggle = () => {
   const [mounted, setMounted] = useState(false)
-
   const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return ( 
     <DropdownMenu>
