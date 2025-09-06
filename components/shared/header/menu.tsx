eb8e0ca0a59465ee646b61b60cfb4f4bb0450224
Nimira43 <n.rabone72@aol.com>
2025-09-06 21:35:43 +0100

    Added ModeToggle and button to sheet menu

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -3,7 +3,7 @@ import ModeToggle from './mode-toggle'
 import Link from 'next/link'
 import { PiShoppingCart, PiUserCircle } from 'react-icons/pi'
 import { IoEllipsisVerticalOutline } from 'react-icons/io5'
-import { Sheet, SheetTrigger } from '@/components/ui/sheet'
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 
 const Menu = () => {
   return ( 
@@ -24,8 +24,23 @@ const Menu = () => {
       <nav className='md:hidden'>
         <Sheet>
           <SheetTrigger className='align-middle'>
-            <
+            <IoEllipsisVerticalOutline />
           </SheetTrigger>
+          <SheetContent className='flex flex-col items-start'>
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+            <Button asChild variant='ghost'>
+              <Link href='/cart'>
+                <PiShoppingCart /> Cart
+              </Link>
+            </Button>
+            <Button asChild variant='ghost'>
+              <Link href='/sign-in'>
+                <PiUserCircle /> Login
+              </Link>
+            </Button>
+            <SheetDescription></SheetDescription>
+          </SheetContent>
         </Sheet>
       </nav>
     </div>
