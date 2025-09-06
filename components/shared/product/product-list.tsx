e4ddbcb62bdfec1400feee6c7b9b3010f1411f02
Nimira43 <n.rabone72@aol.com>
2025-09-06 22:06:17 +0100

    Setting up products grid or no products found terniary

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -10,6 +10,15 @@ const ProductList = ({
       <h2 className='h2-bold mb-4'>
         {title}
       </h2>
+      { data.length > 0 ? (
+        <div className='grid grid-cols-1'>
+          Product Grid
+        </div>
+      ) : (
+        <div>
+          <p>No products found.</p>
+        </div>
+      )}
     </div>
    )
 }
