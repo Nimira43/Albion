40b5d092f687f0116ae3fab3af2088d2202ecc02
Nimira43 <n.rabone72@aol.com>
2025-09-06 20:02:00 +0100

    Brought in Dropdown menu components and icons

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,12 +1,24 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuContent } from '@/components/ui/dropdown-menu'
+import { PiSun, PiMoon } from 'react-icons/pi'
+import { LuSunMoon } from 'react-icons/lu'
 import { useTheme } from 'next-themes'
 
 const ModeToggle = () => {
   const { theme, setTheme } = useTheme()
 
   return ( 
-    <>Toggle</>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='ghost'
+        >
+
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
    )
 }
  
