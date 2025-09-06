4d9cd46f06d094baddbede1d7d3be0918060312d
Nimira43 <n.rabone72@aol.com>
2025-09-06 22:38:42 +0100

    Setting up product image

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -11,6 +11,13 @@ const ProductCard = ({
     <Card className='w-full max-w-sm'>
       <CardHeader className='p-0 items-center'>
         <Link href={`/product/${product.slug}`} />
+        <Image 
+          src={product.images[0]}
+          alt={product.name}
+          height={300}
+          width={300}
+          priority={true}
+        />
       </CardHeader>
     </Card>
    )
