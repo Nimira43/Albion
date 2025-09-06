426d6e03c7621c13625f98d2839b8b50a9c7d879
Nimira43 <n.rabone72@aol.com>
2025-09-06 22:49:19 +0100

    Corrected product image link.

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -10,14 +10,15 @@ const ProductCard = ({
   return ( 
     <Card className='w-full max-w-sm'>
       <CardHeader className='p-0 items-center'>
-        <Link href={`/product/${product.slug}`} />
-        <Image 
-          src={product.images[0]}
-          alt={product.name}
-          height={300}
-          width={300}
-          priority={true}
-        />
+        <Link href={`/product/${product.slug}`}>
+          <Image 
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            priority={true}
+          />
+        </Link>
       </CardHeader>
       <CardContent className='p-4 grid gap-4'>
         <div className='text-xs'>{product.brand}</div>
