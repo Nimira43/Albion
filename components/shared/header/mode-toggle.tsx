2b16d043208a1dd438c75bded4b02542f747c148
Nimira43 <n.rabone72@aol.com>
2025-09-06 19:56:48 +0100

    useTheme variable

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,6 +1,12 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+
 const ModeToggle = () => {
+  const { theme, setTheme } = useTheme()
+
   return ( 
-    <div>Toggle</div>
+    <>Toggle</>
    )
 }
  
