b3f449c384e1d7f26885301182fed4969fd216e9
Nimira43 <n.rabone72@aol.com>
2025-09-06 18:36:31 +0100

    Cleaning up HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,6 @@
-import { Button } from "@/components/ui/button"
-
 const HomePage = () => {
   return ( 
-    <>
-      <div className='p-6 bg-support'>
-        <h1 className='text-3xl text-light app-name'>Albion</h1>
-      </div>
-        <div className='mt-6 p-6'>
-          <Button className='uppercase'>Login</Button>
-        </div>
-    </>
+   <div>Albion</div>
     
   )
 }
