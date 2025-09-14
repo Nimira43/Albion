5f34afebc2a37d3c38117a6c832edf7c4c7ebc18
Nimira43 <n.rabone72@aol.com>
2025-09-15 00:04:26 +0100

    Imported Product type into ProductCard and passed it as product type

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,11 +2,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import ProductPrice from './product-price'
+import { Product } from '@/types'
 
 const ProductCard = ({
   product
 }: {
-  product: any
+  product: Product
 }) => {
   return ( 
     <Card className='w-full max-w-sm'>
