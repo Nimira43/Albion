c9968999be34a53921d88080526c181602a2a82b
Nimira43 <n.rabone72@aol.com>
2025-09-14 23:57:46 +0100

    Added regex

--- lib/validators.ts
+++ lib/validators.ts
@@ -13,5 +13,5 @@ export const insertProductSchema = z.object({
   banner: z.string().nullable(),
   price: z
     .string()
-    .refine((value) => /regex/.test(formatNumberWithDecimal(Number(value))) )
+    .refine((value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),)
 })
