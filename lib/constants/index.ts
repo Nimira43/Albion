06d523f466cd66e395daa0ba97e30d358596ca1c
Nimira43 <n.rabone72@aol.com>
2025-09-14 22:29:13 +0100

    Created latest products limit variable

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,8 @@
+
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ||'Albion'
 
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION ||'E-commerce website using Next JS 15, TypeScript, Tailwind, Shadcn, Prisma and PostgreSQL'
 
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT || 4) 
