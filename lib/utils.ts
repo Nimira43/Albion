28c07eabdb0fb94b08ca3838a5eed03e4824d655
Nimira43 <n.rabone72@aol.com>
2025-09-14 23:50:04 +0100

    formatNumberWithDecimal - 3

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,5 +11,7 @@ export function convertToPlainObject<T>(value: T): T {
 
 export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split('.')
-  return decimal ? `${int}.${decimal.padEnd(2, '0')}`
+  return decimal 
+    ? `${int}.${decimal.padEnd(2, '0')}` 
+    : `${int}.00`
 }
