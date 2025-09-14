a30f06713a7f9bee90904cf3cb44eee9f76e3bfe
Nimira43 <n.rabone72@aol.com>
2025-09-15 00:08:06 +0100

    Passing down Product type where needed.

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -6,7 +6,7 @@ const ProductList = ({
   title,
   limit
 }: { 
-  data: any
+  data: Product[]
   title?: string
   limit?: number
 }) => {
@@ -19,7 +19,7 @@ const ProductList = ({
       </h2>
       {data.length > 0 ? (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-40'>
-          {limitedData.map((product: any) => (
+          {limitedData.map((product: Product) => (
             <ProductCard  
               key={product.slug}
               product={product}
