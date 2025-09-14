f1b75061b7dd93aa555be6e1a8fae54edbac03f7
Nimira43 <n.rabone72@aol.com>
2025-09-15 00:02:07 +0100

    ...and added currency to price

--- lib/validators.ts
+++ lib/validators.ts
@@ -18,10 +18,5 @@ export const insertProductSchema = z.object({
   images: z.array(z.string()).min(1, 'Product must have at least 1 image.'),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
-  price: z
-    .string()
-    .refine(
-      (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))), 
-      'Price must have exactly two decimal place.'
-    )
+  price: currency
 })
