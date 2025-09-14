a816727119aa794fb93967d57d910b7358bcf98c
Nimira43 <n.rabone72@aol.com>
2025-09-14 23:24:29 +0100

    insertProductSchema - 4

--- lib/validators.ts
+++ lib/validators.ts
@@ -6,4 +6,6 @@ export const insertProductSchema = z.object({
   category: z.string().min(3, 'Category must be at least 3 characters.'),
   brand: z.string().min(3, 'Brand must be at least 3 characters.'),
   description: z.string().min(3, 'Description must be at least 3 characters.'),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, 'Product must have at least 1 image.')
 })
