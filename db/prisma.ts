0b85da8ddece6254b7e61fec2d8a07438de193e0
Nimira43 <n.rabone72@aol.com>
2025-09-24 16:18:04 +0100

    Stabilised Prisma and Neon

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,4 +1,4 @@
-import { Pool, neonConfig } from '@neondatabase/serverless'
+import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
 import ws from 'ws'
@@ -6,8 +6,7 @@ import ws from 'ws'
 neonConfig.webSocketConstructor = ws
 const connectionString = `${process.env.DATABASE_URL}`
 
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
+const adapter = new PrismaNeon({ connectionString })
 
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
