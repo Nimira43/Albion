784be16977d6ad1145a526ab894f6797225957bf
Nimira43 <n.rabone72@aol.com>
2025-09-28 21:08:27 +0100

    Getting product

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,9 +1,30 @@
-const ProductDetailsPage = (props: {
-  params: Promise<{ slug: string }>
+import { getProductBySlug } from '@/lib/actions/product.actions'
+import { notFound } from 'next/navigation'
+
+// const ProductDetailsPage = async (props: {
+//   params: Promise<{ slug: string }>
+// }) => {
+//   const { slug } = await props.params
+//   const product = await getProductBySlug(slug)
+
+//   if (!product) notFound()
+    
+//   return ( 
+//     <>{ product.name }</>
+//    )
+// }
+
+const ProductDetailsPage = async ({
+  params
+}: {
+  params: { slug: string }
 }) => {
-  return ( 
-    <div>Product Details Page</div>
-   )
+  const product = await getProductBySlug(params.slug)
+
+  if (!product) notFound()
+
+  return <>{product.name}</>
 }
+
  
 export default ProductDetailsPage
