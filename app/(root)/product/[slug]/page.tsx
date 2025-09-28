1d17fb874478fffd4c70023bd00f9ec331a124f4
Nimira43 <n.rabone72@aol.com>
2025-09-28 21:20:14 +0100

    ProductDetailsPage layout and styles - 1

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,32 @@ import { getProductBySlug } from '@/lib/actions/product.actions'
 import { notFound } from 'next/navigation'
 import ProductPrice from '@/components/shared/product/product-price'
 
+const ProductDetailsPage = async ({
+  params
+}: {
+  params: { slug: string }
+}) => {
+  const product = await getProductBySlug(params.slug)
+
+  if (!product) notFound()
+
+  return (
+    <>
+      <section>
+        <div className='grid grid-cols-1 md:grid-cols-5'>
+          
+        </div>
+      </section>
+    
+    </>
+  )
+}
+
+export default ProductDetailsPage
+
+
+/*
+
 // const ProductDetailsPage = async (props: {
 //   params: Promise<{ slug: string }>
 // }) => {
@@ -18,16 +44,4 @@ import ProductPrice from '@/components/shared/product/product-price'
 //    )
 // }
 
-const ProductDetailsPage = async ({
-  params
-}: {
-  params: { slug: string }
-}) => {
-  const product = await getProductBySlug(params.slug)
-
-  if (!product) notFound()
-
-  return <>{product.name}</>
-}
-
-export default ProductDetailsPage
+*/
