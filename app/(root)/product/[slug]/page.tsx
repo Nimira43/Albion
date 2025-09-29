aa65a4e0bb6fbbf469b684f7d41d644e00b20f46
Nimira43 <n.rabone72@aol.com>
2025-09-29 01:20:32 +0100

    Layout - 5

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -42,7 +42,20 @@ const ProductDetailsPage = async ({
             <Card>
               <CardContent className='p-4'>
                 <div className='mb-2 flex justify-between'>
-                  
+                  <div>Price</div>
+                  <div>
+                    <ProductPrice 
+                      value={Number(product.price)}
+                    />
+                  </div>
+                </div>
+                <div className='mb-2 flex justify-between'>
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge variant='outline'>In Stock</Badge>
+                  ) : (
+                    <Badge variant='destructive'>Out of Stock</Badge>
+                  )}
                 </div>
               </CardContent>
             </Card>
