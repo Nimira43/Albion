72f8ee02c6fd5d15ceccf73f17993b35d597a762
Nimira43 <n.rabone72@aol.com>
2025-10-07 09:00:03 +0100

    Made ProductImages use client. Brought in useState, Image and cn

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -1,4 +1,10 @@
+'use client'
+import { useState } from 'react'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+
 const ProductImages = ({images} : {images: string[]}) => {
+  console.log(images)
   return ( 
     <>
       Images
