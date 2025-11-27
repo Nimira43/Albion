b812cc020d3b29e9aae9c1bc66b2fd249682e911
Nimira43 <n.rabone72@aol.com>
2025-11-27 01:08:18 +0000

    config pages

--- auth.ts
+++ auth.ts
@@ -1,5 +1,11 @@
 import NextAuth from 'next-auth'
 
-export const config = {}
+export const config = {
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
+  
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
