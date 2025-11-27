f60aaa2d38efe6cdedc0bb20993347f109c16be2
Nimira43 <n.rabone72@aol.com>
2025-11-27 01:11:59 +0000

    config - session and imports

--- auth.ts
+++ auth.ts
@@ -1,11 +1,18 @@
 import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/db/prisma'
 
 export const config = {
   pages: {
     signIn: '/sign-in',
     error: '/sign-in',
   },
-  
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60,
+
+  }
+
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
