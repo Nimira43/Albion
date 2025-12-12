da2f3eca1214fc4b9049c26cfcb1576eafa70cad
Nimira43 <n.rabone72@aol.com>
2025-12-12 12:55:18 +0000

    signInWithCredentials - 3

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -7,4 +7,20 @@ import { isRedirectError } from 'next/dist/client/components/redirect-error'
 export async function signInWithCredentials(
   prevState: unknown,
   formData: FormData
-) {}
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password')
+    })
+
+    await signIn('credentials', user)
+
+    return {
+      success: true,
+      message: 'Signed in successfully'
+    }
+  } catch (error) {
+    
+  }
+}
