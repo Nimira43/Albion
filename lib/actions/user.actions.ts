bd6733a05a9eb0359962e050467768744f7ca0bb
Nimira43 <n.rabone72@aol.com>
2025-12-12 12:58:23 +0000

    signInWithCredentials - 5

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -24,5 +24,10 @@ export async function signInWithCredentials(
     if (isRedirectError(error)) {
       throw error
     }
+
+    return {
+      success: false,
+      message: 'Invalid email or password'
+    }
   }
 }
