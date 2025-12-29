fef4cbf473e83b583fd528dc83bf825200604b69
Nimira43 <n.rabone72@aol.com>
2025-12-29 18:21:28 +0000

    Logo image brought in.

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,15 +1,30 @@
+import { Card, CardHeader } from '@/components/ui/card'
+import { APP_NAME } from '@/lib/constants'
 import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
-  title: 'Sign In'
+  title: 'Login'
 }
-
 
 const SignIn = () => {
   return ( 
-    <>
-      Sign In
-    </>
+    <div className='w-full max-w-md mx-auto'>
+      <Card>
+        <CardHeader className='space-y-4'>
+          <Link href='/' className='flex-center'>
+            <Image
+              src='/images/logo1.png' 
+              alt={`${APP_NAME} logo`}
+              width={100}
+              height={100}
+            />
+          
+          </Link>
+        </CardHeader>
+      </Card>
+    </div>
   )
 }
  
