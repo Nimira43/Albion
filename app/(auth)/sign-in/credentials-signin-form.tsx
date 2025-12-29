e95669ef099f24d8f79f03f4d043980b812f400c
Nimira43 <n.rabone72@aol.com>
2025-12-29 18:33:51 +0000

    Email label and input

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,8 +1,16 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 const CredentialsSignInForm = () => {
   return ( 
-    <>
-      
-    </>
+    <form>
+      <div className='space-y-6'>
+        <div>
+          <Label htmlFor='email'>Email</Label>
+          <Input />
+        </div>
+      </div>
+    </form>
    )
 }
  
