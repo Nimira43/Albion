b229bcb29d410870a05cda70e6a4bee24bea4eaa
Nimira43 <n.rabone72@aol.com>
2026-05-02 05:57:56 +0100

    Add to cart button functionality.

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,12 +2,19 @@
 
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
-import { CartItem } from '@/types'
+import { Cart, CartItem } from '@/types'
 import { useToast } from '@/hooks/use-toast'
 import { ToastAction } from '@/components/ui/toast'
-import { addItemToCart } from '@/lib/actions/cart.action'
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.action'
+import { TbPlus, TbMinus } from 'react-icons/tb'
 
-const AddToCart = ({ item }: {item: CartItem }) => {
+const AddToCart = ({
+  cart,
+  item 
+}: {
+  cart?: Cart,
+  item: CartItem
+}) => {
   const router = useRouter()
   const { toast } = useToast()
   
@@ -36,14 +43,48 @@ const AddToCart = ({ item }: {item: CartItem }) => {
     })
   }
 
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId)
+    toast({
+      variant: res.success ? 'default' : 'destructive',
+      description: res.message
+    })
+    return
+  }
+
+  const existItem = cart && cart.items.find((x) => x.productId === item.productId)
+
   return (  
-    <Button
-      className='w-full'
-      type='button'
-      onClick={handleAddToCart}
-    >
-      Add to Cart
-    </Button>
+    existItem ?
+      (
+        <div>
+          <Button
+            type='button'
+            variant='outline'
+            onClick={handleRemoveFromCart}
+          >
+            <TbMinus className='h-4 w-4' />
+          </Button>
+          <span className='px-2'>
+            {existItem.qty}
+          </span>
+          <Button
+            type='button'
+            variant='outline'
+            onClick={handleAddToCart}
+          >
+            <TbPlus className='h-4 w-4' />
+          </Button>
+        </div>
+      ): (
+        <Button
+          className='w-full'
+          type='button'
+          onClick={handleAddToCart}
+        >
+          <TbPlus /> Add to Cart
+        </Button >
+      )
   )
 }
  
