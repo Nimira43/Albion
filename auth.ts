87e2e7ff0e604f207c36923aefc2e7f555708078
Nimira43 <n.rabone72@aol.com>
2026-06-04 17:27:39 +0100

    Updated auth.ts

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import { compareSync } from 'bcrypt-ts-edge'
 import type { NextAuthConfig } from 'next-auth'
 import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
 
 export const config = {
   pages: {
@@ -53,8 +54,6 @@ export const config = {
       session.user.role = token.role
       session.user.name = token.name
 
-      console.log(token)
-
       if (trigger === 'update') {
         session.user.name = user.name
       }
@@ -63,16 +62,45 @@ export const config = {
     },
     async jwt({ token, user, trigger, session }: any) {
       if (user) {
+        token.id = user.id
         token.role = user.role
 
         if (user.name === 'NO_NAME') {
           token.name = user.email!.split('@')[0]
+          
           await prisma.user.update({
             where: {id: user.id},
             data: {name: token.name}
           })
         }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies()
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId }
+            })
+
+            if (sessionCart) {
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              })
+
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: {userId: user.id}
+              })
+            }
+          }
+        }
       }
+
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name
+      }
+
       return token
     },
     authorized({ request, auth }: any) {
