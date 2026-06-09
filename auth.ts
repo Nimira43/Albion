595026d1703948ccbe759153866b4a0042b07ee4
Nimira43 <n.rabone72@aol.com>
2026-06-09 04:53:22 +0100

    protectedPaths

--- auth.ts
+++ auth.ts
@@ -104,6 +104,20 @@ export const config = {
       return token
     },
     authorized({ request, auth }: any) {
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ]
+
+      const { pathname } = request.nextUrl
+      
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false
+
       if (!request.cookies.get('sessionCartId')) {
         const sessionCartId = crypto.randomUUID()
         const newRequestHeaders = new Headers(request.headers)
