ce0c08c487ba3bb9cd78f9cc72c199201f7e0c13
Nimira43 <n.rabone72@aol.com>
2026-07-14 19:25:51 +0100

    Place Order button.

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -11,6 +11,7 @@ import { Button } from '@/components/ui/button'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import Image from 'next/image'
 import { formatCurrency } from '@/lib/utils'
+import PlaceOrderForm from './place-order-form'
 
 export const metadata: Metadata = {
   title: 'Place Order',
@@ -165,6 +166,7 @@ const PlaceOrderPage = async () => {
                   {formatCurrency(cart.totalPrice)}
                 </div>
               </div>
+              <PlaceOrderForm />
             </CardContent>
           </Card>
         </div>
