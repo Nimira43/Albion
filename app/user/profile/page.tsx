dd0467abf2e1fbc68153042926eef535d303d202
Nimira43 <n.rabone72@aol.com>
2026-09-18 01:05:53 +0100

    Update profile form.

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,8 +1,24 @@
-const Profile = () => {
+import { Metadata } from 'next'
+import { auth } from '@/auth'
+import { SessionProvider } from 'next-auth/react'
+import ProfileForm from '../orders/profile-form'
+
+export const metadata: Metadata = {
+  title: 'Customer Profile'
+}
+
+const Profile = async () => {
+  const session = await auth()
+
   return (  
-    <div>
-      Profile
-    </div>
+    <SessionProvider session={session}>
+      <div className='max-w-md mx-auto space-y-4'>
+        <h2 className='h2-bold'>
+          Profile
+        </h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
   )
 }
  
